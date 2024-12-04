014f231ff5c0675f1f5fccccc93676830c344660
Nam Nguyen <namhnguyen0103@gmail.com>
2024-12-04 16:39:21 -0500

    Solve medium jump game II

---
+++ Medium/45-jump-game-II.ts
@@ -0,0 +1,48 @@
+/* 
+45. Jump Game II
+
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if 
+you are at nums[i], you can jump to any nums[i + j] where:
+
+    * 0 <= j <= nums[i] and
+    * i + j < n
+
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+ 
+
+Example 1:
+
+    Input: nums = [2,3,1,1,4]
+    Output: 2
+    Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+    Input: nums = [2,3,0,1,4]
+    Output: 2
+
+*/
+
+function jump(nums: number[]): number {
+    if (nums.length === 1) return 0;
+    const goal = nums.length - 1;
+    let jumps = 0;
+    let furthest = 0;
+    let fix = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        const currentJump = i + nums[i];
+        if (currentJump >= goal) return ++jumps;
+        if (currentJump > furthest) {
+            furthest = currentJump;
+        }
+        if (i >= fix) {
+            fix = furthest;
+            jumps++;
+        }
+    }
+    return jumps;
+};
